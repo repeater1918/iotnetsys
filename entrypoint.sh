b96feb84257a12f7a986785d84945318fa11b7b7
JM-Fxd <jonomacdee@gmail.com>
2023-05-18 16:48:03 +1000

    integrate SSL into CICD

---
+++ entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Start Nginx server
+nginx -g "daemon off;" &
+
+# Run certbot command for SLL
+certbot --nginx -d iotnetsys.net --agree-tos --email jomc7031@uni.sydney.edu.au --non-interactive
